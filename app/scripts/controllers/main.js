171d76c82f7d76b8a3f2bde10952c2d40242b78a
Matt Stabeler <matt@bigbadweb.co.uk>
2015-01-21 09:26:29 +0000

    fix buttons show timer

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,26 +11,28 @@ angular.module('ftaTimerApp')
   .controller('MainCtrl', function ($scope) {
 
     $scope.thingsIMightBeDoing = [
+      'Not putting teaspoons in the toilet',
       'Not doing something strange Men\'s loo',
       'Reverse parking',
-      'Not putting the wrong plastic in the wrong place'
+      'Not putting the wrong plastic in the wrong place',
+      'Not bumping my head on low hanging walls'
     ];
 
     $scope.startTime = null;
 
     $scope.totalTime = null;
 
-    $scope.selectedTask = null;
+    $scope.selectedTask = $scope.thingsIMightBeDoing[0];
     $scope.previousTask = null;
 
     $scope.toggleTimer = function(){
     	if($scope.startTime){
         $scope.previousTask = $scope.selectedTask || 'Nothing in particular';
-    		$scope.totalTime = Math.floor(Date.now() / 1000) - $scope.startTime;
+    		$scope.totalTime = Math.floor(Date.now() / 1000) - Math.floor($scope.startTime / 1000);
     		$scope.startTime = null;
     	}else{
     		$scope.totalTime = null;
-    		$scope.startTime = Math.floor(Date.now() / 1000);
+    		$scope.startTime = Date.now();
     	}
     };
 
